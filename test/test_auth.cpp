87a58a240ae15dc74cc71b7b73f6fe79bd62cc4d
Mikael Kilpeläinen <mikael@securepath.fi>
2022-05-14 00:02:03 +0300

    improve ssh test

---
+++ test/test_auth.cpp
@@ -0,0 +1,17 @@
+
+#include "crypto.hpp"
+#include "random.hpp"
+#include "test_buffers.hpp"
+#include "test/util/server_auth_service.hpp"
+
+namespace securepath::ssh::test {
+namespace {
+
+}
+
+
+TEST_CASE("password auth test", "[unit][crypto][auth]") {
+
+}
+
+}
--- test/test_ssh.cpp
+++ test/test_ssh.cpp
@@ -5,42 +5,61 @@
 #include "ssh/crypto/private_key.hpp"
 #include "ssh/client/ssh_client.hpp"
 #include "ssh/server/ssh_server.hpp"
+#include "ssh/server/server_config.hpp"
+#include "test/util/server_auth_service.hpp"
 
 namespace securepath::ssh::test {
 namespace {
 struct test_context {
-	test_context(logger& l, std::string tag, ssh_config c = {}, bool is_client = true)
-	: slog(l, tag), config(std::move(c))
+	test_context(logger& l, std::string tag)
+	: slog(l, tag)
 	{
-		// make sure we have correct side set
-		if(is_client) {
-			config.side = transport_side::client;
-		} else {
-			config.side = transport_side::server;
-		}
 	}
 
 	session_logger slog;
-	client_config config;
 	string_io_buffer out_buf;
 };
 
-struct test_client : test_context, ssh_client {
-	test_client(logger& l, ssh_config c = {})
-	: test_context(l, "[client] ", std::move(c), true), ssh_client(test_context::config, slog, out_buf)
+struct test_client : test_context, client_config, ssh_client {
+	test_client(logger& l, client_config c = {})
+	: test_context(l, "[client] ")
+	, client_config{std::move(c)}
+	, ssh_client(*this, slog, out_buf)
 	{
+		side = transport_side::client;
+	}
+
+	void set_test_auth() {
+		username = "test";
+		password = "some";
+		service = "test-service";
 	}
 };
 
-struct test_server : test_context, ssh_server {
+struct test_server : test_context, server_config, ssh_server {
 	test_server(logger& l, ssh_config c = {})
-	: test_context(l, "[server] ", std::move(c), false), ssh_server(test_context::config, slog, out_buf)
+	: test_context(l, "[server] ")
+	, server_config{std::move(c)}
+	, ssh_server(*this, slog, out_buf)
 	{
+		side = transport_side::server;
 	}
 
 	std::unique_ptr<ssh_service> construct_service(std::string_view name) override {
+		if(name == user_auth_service_name) {
+			return std::make_unique<server_test_auth_service>(*this, auth, std::move(auth_data));
+		}
+
 		return nullptr;
 	}
+
+	void set_test_auth() {
+		//void add_pk(std::string const& user, std::string fp)
+		auth_data.add_pk("test", "some");
+		//void add_password(std::string const& user, std::string password);
+	}
+
+	test_auth_data auth_data;
 };
 }
 
@@ -58,8 +77,8 @@ bool run(test_client& client, test_server& server) {
 	return client.error() == 0 && server.error() == 0;
 }
 
-static ssh_config test_client_config() {
-	ssh_config c;
+static client_config test_client_config() {
+	client_config c;
 	c.algorithms.host_keys = {key_type::ssh_ed25519};
 	c.algorithms.kexes = {kex_type::curve25519_sha256};
 	c.algorithms.client_server_ciphers = {cipher_type::aes_256_gcm};
@@ -72,8 +91,8 @@ static ssh_config test_client_config() {
 	return c;
 }
 
-static ssh_config test_server_config() {
-	ssh_config c;
+static server_config test_server_config() {
+	server_config c;
 	c.algorithms.host_keys = {key_type::ssh_ed25519};
 	c.algorithms.kexes = {kex_type::curve25519_sha256};
 	c.algorithms.client_server_ciphers = {cipher_type::aes_256_gcm};
@@ -109,9 +128,9 @@ TEST_CASE("ssh test", "[unit]") {
 }
 
 TEST_CASE("ssh test guess", "[unit]") {
-	ssh_config s = test_server_config();
+	server_config s = test_server_config();
 	s.guess_kex_packet = true;
-	ssh_config c = test_client_config();
+	client_config c = test_client_config();
 	c.guess_kex_packet = true;
 	test_server server(test_log(), std::move(s));
 	test_client client(test_log(), std::move(c));
@@ -124,7 +143,8 @@ TEST_CASE("ssh test guess", "[unit]") {
 
 TEST_CASE("ssh failing version exchange", "[unit]") {
 	test_server server(test_log());
-	test_client client(test_log(), ssh_config{.my_version = ssh_version{.ssh="1.0"}});
+	client_config c = test_client_config();
+	test_client client(test_log(), client_config{ssh_config{.my_version = ssh_version{.ssh="1.0"}}});
 
 	CHECK(!run(client, server));
 
@@ -133,6 +153,20 @@ TEST_CASE("ssh failing version exchange", "[unit]") {
 	// the ssh_transport tries to send kexinit before reading the disconnect packet
 	CHECK(client.error() != ssh_error_code::ssh_noerror);
 	CHECK(server.error() == ssh_error_code::ssh_protocol_version_not_supported);
+}
+
+
+TEST_CASE("ssh failing auth", "[unit]") {
+	test_server server(test_log());
+	test_client client(test_log());
+
+	CHECK(!run(client, server));
+
+	CHECK(client.state() == ssh_state::disconnected);
+	CHECK(server.state() == ssh_state::disconnected);
+
+	CHECK(client.error() == ssh_error_code::ssh_no_more_auth_methods_available);
+	CHECK(server.error() == ssh_error_code::ssh_no_more_auth_methods_available);
 }
 
 }
